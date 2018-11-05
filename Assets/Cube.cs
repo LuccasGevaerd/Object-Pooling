d8a3e65338f94a32e1751847c6fb90946a0fbffb
Luccas Gevaerd <luccasgoroberto@gmail.com>
2018-11-05 15:39:38 -0200

    Update do projeto

---
+++ Assets/Cube.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour, IPooledObject {
+    public float up = 1f;
+    public float side = 1f;
+
+	// Use this for initialization
+	public void OnObjectSpawn () {
+        float xForce = Random.Range(-side, side);
+        float yForce = Random.Range(up / 2f, up);
+        float zForce = Random.Range(-side, side);
+
+        Vector3 force = new Vector3(xForce, yForce, zForce);
+
+        GetComponent<Rigidbody>().velocity = force;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Spawner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    ObjectPooler objectPooler;
+    private void Start()
+    {
+        objectPooler = ObjectPooler.Instance;
+    }
+    // Update is called once per frame
+    void FixedUpdate () {
+        objectPooler.SpawnerCube("Cube", transform.position, Quaternion.identity);
+	}
+}
